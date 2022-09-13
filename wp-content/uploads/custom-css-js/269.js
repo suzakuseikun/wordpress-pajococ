7aafa9cd0f2dac104d06275553105c2975a8dc63
Misael Paul Mamangun <misaelpaulmamangun@gmail.com>
2022-09-13 22:22:45 +0800

    first commit

---
+++ wp-content/uploads/custom-css-js/269.js
@@ -0,0 +1,10 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+/* Default comment here */ 
+
+jQuery(document).ready(function(){
+    jQuery("footer span.copyright").prependTo("footer .inside-site-info");
+});
+
+</script>
+<!-- end Simple Custom CSS and JS -->
